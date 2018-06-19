c1fd931bcb6e3dd747bbdf06bb97cbda6bf1e0ba
tsky <thomas.kollakowsky@gmail.com>
2018-06-19 16:27:24 +0200

    update

--- setVCPGK.bat
+++ setVCPGK.bat
@@ -1,15 +1,5 @@
-:: set enviroment dir with ThirdParty Path
-echo off
 
-for %%i in ("%~dp0..") do set "PARENTDIR=%%~fi"
-echo %PARENTDIR%
-
-SET CURRENTDIR=%~dp0
-
-echo %PARENTDIR%
-echo %CURRENTDIR%
-
-SET _VCPKG_ROOT=%PARENTDIR%
+SET PARENTDIR=%VCPKG_ROOT%
 
 SET _VCPKG_BIN=%PARENTDIR%\installed\x64-windows\bin
 SET _VCPKG_DEBUG_BIN=%PARENTDIR%\installed\x64-windows\debug\bin
@@ -21,13 +11,12 @@ SET _VCPKG_DEBUG_LIB_MANUAL_LINK=%PARENTDIR%\installed\x64-windows\debug\lib\man
 SET _VCPKG_SHARE=%PARENTDIR%\installed\x64-windows\share
 SET _VCPKG_TOOLS=%PARENTDIR%\installed\x64-windows\tools
 
-echo "vcpkg root " %_VCPKG_ROOT%
+echo "vcpkg root " %VCPKG_ROOT%
 echo "vcpkg include " %_VCPKG_INCLUDE%
 echo "vcpkg bin " %_VCPKG_BIN%
 echo "vcpkg lib " %_VCPKG_LIB%
 echo "vcpkg tools " %_VCPKG_TOOLS%
 
-setx VCPKG_ROOT %_VCPKG_ROOT% 
 setx VCPKG_BIN %_VCPKG_BIN% 
 setx VCPKG_DEBUG_BIN %_VCPKG_DEBUG_BIN%
 setx VCPKG_INCLUDE %_VCPKG_INCLUDE% 
@@ -38,6 +27,7 @@ setx VCPKG_DEBUG_LIB_MANUAL_LINK %_VCPKG_DEBUG_LIB_MANUAL_LINK%
 setx VCPKG_SHARE %_VCPKG_SHARE%
 setx VCPKG_TOOLS %_VCPKG_TOOLS%
 
+setx VCPKG_DEFAULT_TRIPLET x64-windows
 echo %PATH% > savepath.txt
 
 echo "set path"
